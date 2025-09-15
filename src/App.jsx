1c634e7f404af6a61bb7382e7ea0e29afe45da06
Gabriela Edwin <00gabrielaedwin00@gmail.com>
2025-09-15 16:48:44 -0300

    NavegandoLasrutasEdwin

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <NavBar />
       <Routes>
         <Route path="/" element={<ItemListContainer greeting="Catálogo completo" />} />
-        <Route path="/category/:categoryId" element={<ItemListContainer greeting="Filtrado por categoría" />} />
+        <Route path="/category/:categoryId" element={<ItemListContainer greeting="Categorias" />} />
         <Route path="/item/:id" element={<ItemDetailContainer />} />
         <Route path="*" element={<h2 style={{ textAlign: "center" }}>404 - Página no encontrada</h2>} />
       </Routes>
