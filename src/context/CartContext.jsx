6a8b70fa388604f7769a35da1bccb49caa4fb330
Gabriela Edwin <00gabrielaedwin00@gmail.com>
2025-10-16 19:27:04 -0300

    arreglando

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,73 +1,57 @@
 import { createContext, useState, useContext } from 'react';
 
-
 export const CartContext = createContext();
-
-
 export const useCartContext = () => useContext(CartContext);
 
-
 export const CartContextProvider = ({ children }) => {
-    const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState([]);
 
-    
-    const addItem = (item, quantity) => {
-    
-        const itemToAdd = { ...item, quantity };
-        
-      
-        const index = cart.findIndex(prod => prod.id === item.id);
+  const addItem = (item, quantity) => {
+    const itemToAdd = { ...item, quantity };
+    const index = cart.findIndex(prod => prod.id === item.id);
 
-        if (index > -1) {
-          
-            const newCart = [...cart];
-            newCart[index].quantity += quantity;
-            setCart(newCart);
-        } else {
-         
-            setCart([...cart, itemToAdd]);
-        }
-    };
+    if (index > -1) {
+      const newCart = [...cart];
+      newCart[index].quantity += quantity;
+      setCart(newCart);
+    } else {
+      setCart([...cart, itemToAdd]);
+    }
+  };
 
-  
-    const removeItem = (id) => {
-        setCart(cart.filter(prod => prod.id !== id));
-    };
+  const removeItem = (id) => {
+    setCart(cart.filter(prod => prod.id !== id));
+  };
 
+  const clear = () => {
+    setCart([]);
+  };
 
-    const clear = () => {
-        setCart([]);
-    };
+  const isInCart = (id) => {
+    return cart.some(prod => prod.id === id);
+  };
 
-   
-    const isInCart = (id) => {
-        return cart.some(prod => prod.id === id);
-    };
+  const totalQuantity = () => {
+    return cart.reduce((acc, prod) => acc + prod.quantity, 0);
+  };
 
-   
-    const totalQuantity = () => {
-        return cart.reduce((acc, prod) => acc + prod.quantity, 0);
-    };
+  const totalPrice = () => {
+    return cart.reduce((acc, prod) => acc + (prod.price * prod.quantity), 0);
+  };
 
-   
-    const totalPrice = () => {
-        return cart.reduce((acc, prod) => acc + (prod.price * prod.quantity), 0);
-    };
+  const contextValue = {
+    cart,
+    addItem,
+    removeItem,
+    clear,
+    isInCart,
+    totalQuantity,
+    totalPrice,
+  };
 
- 
-    const contextValue = {
-        cart,
-        addItem,
-        removeItem,
-        clear,
-        isInCart,
-        totalQuantity,
-        totalPrice,
-    };
-
-    return (
-        <CartContext.Provider value={contextValue}>
-            {children}
-        </CartContext.Provider>
-    );
+  return (
+    <CartContext.Provider value={contextValue}>
+      {children}
+    </CartContext.Provider>
+  );
 };
